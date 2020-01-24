7bd1426e95f2a0b17183565257a7ebc92d2f0098
Travis Reyburn <travisreyburn@gmail.com>
2020-01-23 21:22:46 -0700

    added functionality to read api key from secrets file

---
+++ unimplemented_code/load_key.py
@@ -0,0 +1,14 @@
+def load_key(path):
+    with open(path) as fh:
+        key = fh.read()
+        return key
+
+
+def main():
+    key_p = r'C:\Users\Travis\projects\vehicle-routing-webapp\unimplemented_code\secret.txt'
+    key = load_key(key_p)
+    print(key)
+
+
+if __name__ == '__main__':
+    main()
