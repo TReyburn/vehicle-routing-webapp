1eaa150a18494a2602b8514bf1b70dd6fe2418df
Travis Reyburn <travisreyburn@gmail.com>
2020-01-23 21:23:36 -0700

    added return_solution func to return output as a list - rather than simply using the print_solution func to print to console

--- unimplemented_code/vr_solver.py
+++ unimplemented_code/vr_solver.py
@@ -1,10 +1,16 @@
 """Vehicles Routing Problem (VRP)."""
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
+
+#
+
 from data_model import create_data_model
+from distance_matrix import create_distance_matrix, create_data
+from load_address_csv import load_address_csv
+from load_key import load_key
 
 
-def return_solution(data, manager, routing, solution):
+def print_solution(data, manager, routing, solution):
     """Prints solution on console."""
     max_route_distance = 0
     for vehicle_id in range(data['num_vehicles']):
@@ -24,10 +30,28 @@ def return_solution(data, manager, routing, solution):
     print('Maximum of the route distances: {}m'.format(max_route_distance))
 
 
-def main():
+def return_solution(data, manager, routing, solution):
+    output_list = []
+
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        plan_output = []
+        route_distance = 0
+        while not routing.IsEnd(index):
+            plan_output.append(manager.IndexToNode(index))
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(previous_index, index, vehicle_id)
+        plan_output.append(manager.IndexToNode(index))
+        output_list.append([vehicle_id, str(plan_output).strip('[]'), route_distance])
+    return output_list
+
+
+def cvrp_solve(user_data):
     """Solve the CVRP problem."""
     # Instantiate the data problem.
-    data = create_data_model()
+    # data = create_data_model()
+    data = user_data
 
     # Create the routing index manager.
     manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
@@ -54,7 +78,7 @@ def main():
     routing.AddDimension(
         transit_callback_index,
         0,  # no slack
-        3000,  # vehicle maximum travel distance
+        300000000000,  # vehicle maximum travel distance
         True,  # start cumul to zero
         dimension_name)
     distance_dimension = routing.GetDimensionOrDie(dimension_name)
@@ -62,8 +86,7 @@ def main():
 
     # Setting first solution heuristic.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+    search_parameters.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
 
     # Solve the problem.
     solution = routing.SolveWithParameters(search_parameters)
@@ -71,7 +94,14 @@ def main():
     # Print solution on console.
     if solution:
         return_solution(data, manager, routing, solution)
+    else:
+        return 'No solution.'
 
 
 if __name__ == '__main__':
-    main()
+    key = load_key(r'C:\Users\Travis\projects\vehicle-routing-webapp\unimplemented_code\secret.txt')
+    addresses = load_address_csv(r'C:\Users\Travis\projects\vehicle-routing-webapp\unimplemented_code\address2.csv')
+    data = create_data(addresses, key)
+    distance_matrix = create_distance_matrix(data)
+    my_data = create_data_model(distance_matrix, 3)
+    cvrp_solve(my_data)
