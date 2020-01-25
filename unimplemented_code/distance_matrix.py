5b88be09f8c53e74a1ede22527b5c63fe85f67b0
Travis Reyburn <travisreyburn@gmail.com>
2020-01-25 12:37:05 -0700

    minor formatting for readability

--- unimplemented_code/distance_matrix.py
+++ unimplemented_code/distance_matrix.py
@@ -12,15 +12,19 @@ def create_data(user_address, key):
 def create_distance_matrix(data):
     addresses = data["addresses"]
     api_key = data["API_key"]
+
     # Distance Matrix API only accepts 100 elements per request, so get rows in multiple requests.
     max_elements = 100
-    num_addresses = len(addresses)  # 16 in this example.
+    num_addresses = len(addresses)
+
     # Maximum number of rows that can be computed per request (6 in this example).
     max_rows = max_elements // num_addresses
+
     # num_addresses = q * max_rows + r (q = 2 and r = 4 in this example).
     q, r = divmod(num_addresses, max_rows)
     dest_addresses = addresses
     distance_matrix = []
+
     # Send q requests, returning max_rows rows per request.
     for i in range(q):
         origin_addresses = addresses[i * max_rows: (i + 1) * max_rows]
