fe522fb49aea10b0e0a3344990ae6bd8a82752af
Travis Reyburn <travisreyburn@gmail.com>
2020-01-26 14:47:08 -0700

    added inputs unit testing against validate_inputs func

--- tests/models/data_model.py
+++ tests/models/data_model.py
@@ -1,22 +1,23 @@
 from models.data_model import create_data_model
 from tests.errors.errors import UnitTypeError, UnitValueError
+from models.validate_inputs.data_model_inputs import _validate_inputs
 
 
 def test_data_model() -> bool:
     bad_list_types = [None, True, [[[[[]]]]], [[None]], [[1, 2, 3], [1, 2, 3], [1, 2, 'a']],
-                      [[1, 2, 3], [1, 2, 3], [1, 2, None]], [[1, 2, 3], [1, 2, 3], [1.1, 2.2, 3.3]]]
+                      [[1, 2, 3], [1, 2, 3], [1, 2, None]], [[1, 2, 3], [1, 2, 3], [1.1, 2.2, 3.3]], object]
 
     bad_list_values = [[], [[1, 2, 3], [1, 2, 3], [1, 2]], [[1, 2, 3], [1, 2, 3]],
                        [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[1, 0, 3], [0, 1, 3], [1, 2, 0]],
                        [[0, 2, 5], [2, 0, 8], [0, 8, 0]], [[0, 2, 5], [2, 1, 8], [5, 8, 0]]]
 
-    bad_num_vehicles_types = [None, True, 'A', 1.1, [], {}]
+    bad_num_vehicles_types = [None, True, 'A', 1.1, [], {}, object]
 
     bad_num_vehicles_values = [-1, 0, -100]
 
-    bad_depot_loc_types = [None, True, 2.2, 'A', [], (), {}]
+    bad_depot_loc_types = [None, True, 2.2, 'A', [], (), {}, object]
 
-    bad_depot_loc_values = [-1, 2000000]
+    bad_depot_loc_values = [-1, 2000000, 3]
 
     good_list = [[0, 2, 5], [2, 0, 8], [5, 8, 0]]
 
@@ -26,16 +27,48 @@ def test_data_model() -> bool:
 
     for bad in bad_list_types:
         try:
-            create_data_model(bad, good_num_vehicle, good_depot_loc)
+            _validate_inputs(bad, good_num_vehicle, good_depot_loc)
             raise AssertionError(f'{bad} should cause unit type error for list input')
         except UnitTypeError:
             pass
 
     for bad in bad_list_values:
         try:
-            create_data_model(bad, good_num_vehicle, good_depot_loc)
+            _validate_inputs(bad, good_num_vehicle, good_depot_loc)
             raise AssertionError(f'{bad} should cause UnitValueError for list input')
         except UnitValueError:
             pass
+
+    for bad in bad_num_vehicles_types:
+        try:
+            _validate_inputs(good_list, bad, good_depot_loc)
+            raise AssertionError(f'{bad} should cause unit type error for num_vehicle input')
+        except UnitTypeError:
+            pass
+
+    for bad in bad_num_vehicles_values:
+        try:
+            _validate_inputs(good_list, bad, good_depot_loc)
+            raise AssertionError(f'{bad} should cause UnitValueError for num_vehicle input')
+        except UnitValueError:
+            pass
+
+    for bad in bad_depot_loc_types:
+        try:
+            _validate_inputs(good_list, good_num_vehicle, bad)
+            raise AssertionError(f'{bad} should cause unit type error for depot_loc input')
+        except UnitTypeError:
+            pass
+
+    for bad in bad_depot_loc_values:
+        try:
+            _validate_inputs(good_list, good_num_vehicle, bad)
+            raise AssertionError(f'{bad} should cause UnitValueError for depot_loc input')
+        except UnitValueError:
+            pass
+
+    """
+        Need to add unit tests which test the output of the function too
+    """
 
     return True
