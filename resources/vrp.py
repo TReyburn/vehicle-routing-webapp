c91b0f234a793b349a28407220b665e730caed97
Travis Reyburn <travisreyburn@gmail.com>
2020-01-25 21:08:36 -0700

    adding our vrp resource code

---
+++ resources/vrp.py
@@ -0,0 +1,14 @@
+from flask_restful import Resource
+from models.generate_distance_matrix import gen_dist_matrix
+from models.data_model import create_data_model
+from models.vrp import vrp_solve
+
+
+class VehicleRouting(Resource):
+
+    def __init__(self):
+        self.dist_matrix = gen_dist_matrix(50, 15, 800)
+        self.data_model = create_data_model(self.dist_matrix, 3)
+
+    def get(self):
+        return vrp_solve(self.data_model)
