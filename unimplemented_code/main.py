5cdca07f93cbf6b432d45bf93c4a776a24cfc271
Travis Reyburn <travisreyburn@gmail.com>
2020-01-25 19:09:22 -0700

    edited to reflect name change of vrp_solve func

--- unimplemented_code/main.py
+++ unimplemented_code/main.py
@@ -1,9 +1,10 @@
-from vr_solver import cvrp_solve
+from vr_solver import vrp_solve
 from data_model import create_data_model
 from distance_matrix import create_distance_matrix, create_data
 from load_address_csv import load_address_csv
 from load_key import load_key
 from generate_distance_matrix import gen_dist_matrix
+import timeit
 
 
 def main():
@@ -17,12 +18,12 @@ def main():
         distance_matrix = gen_dist_matrix(50, 15, 900)
     elif debug and asym:
         print('2')
-        distance_matrix = gen_dist_matrix(80, 15, 900, asym=True, asym_max=50)
+        distance_matrix = gen_dist_matrix(100, 15, 900, asym=True, asym_max=50)
     else:
         print('3')
         distance_matrix = create_distance_matrix(data)
     my_data = create_data_model(distance_matrix, 3)
-    output = cvrp_solve(my_data)
+    output = vrp_solve(my_data)
     print(output)
 
 
