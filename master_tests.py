bb30ca287375f6ad243a89b928a54d58b1c72938
Travis Reyburn <travisreyburn@gmail.com>
2020-01-25 21:54:30 -0700

    added data_model test suite

--- master_tests.py
+++ master_tests.py
@@ -1,4 +1,8 @@
 from tests.models.generate_distance_matrix import test_gen_dist_matrix
+from tests.models.data_model import test_data_model
 
 if test_gen_dist_matrix():
-    print('success')
+    print('gen_dist_matrix() tests: success')
+
+if test_data_model():
+    print('create_data_model() tests: success')
