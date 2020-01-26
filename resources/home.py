01c39f71e620790324603eeb8410fd2ae3efddd6
Travis Reyburn <travisreyburn@gmail.com>
2020-01-25 20:10:35 -0700

    added resource for home page

--- resources/home.py
+++ resources/home.py
@@ -1,1 +1,11 @@
+from flask import render_template
+from flask_restful import Resource
 
+
+class HomePage(Resource):
+
+    def __init__(self):
+        self.home_page = 'index.html'
+
+    def get(self):
+        return render_template(self.home_page), 200
