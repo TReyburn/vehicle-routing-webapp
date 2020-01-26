b589175cd36d56c0d696fdab97f520d79d293c2f
Travis Reyburn <travisreyburn@gmail.com>
2020-01-25 18:36:48 -0700

    adding debug flag, asym flag, and implementing random distance matrix generator

--- unimplemented_code/main.py
+++ unimplemented_code/main.py
@@ -3,15 +3,27 @@ from data_model import create_data_model
 from distance_matrix import create_distance_matrix, create_data
 from load_address_csv import load_address_csv
 from load_key import load_key
+from generate_distance_matrix import gen_dist_matrix
 
 
 def main():
+    debug = True
+    asym = False
     key = load_key(r'C:\Users\Travis\projects\vehicle-routing-webapp\unimplemented_code\secret.txt')
     addresses = load_address_csv(r'C:\Users\Travis\projects\vehicle-routing-webapp\unimplemented_code\addresses.csv')
     data = create_data(addresses, key)
-    distance_matrix = create_distance_matrix(data)
+    if debug and not asym:
+        print('1')
+        distance_matrix = gen_dist_matrix(100, 15, 900)
+    elif debug and asym:
+        print('2')
+        distance_matrix = gen_dist_matrix(20, 15, 900, asym=asym, asym_max=50)
+    else:
+        print('3')
+        distance_matrix = create_distance_matrix(data)
     my_data = create_data_model(distance_matrix, 3)
-    cvrp_solve(my_data)
+    output = cvrp_solve(my_data)
+    print(output)
 
 
 if __name__ == '__main__':
